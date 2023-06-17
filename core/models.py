e9968892340f0449ac7b6dd0e0c8236adc3d7ac1
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-06-17 18:09:57 +0330

    add same feature of class in core modules

--- core/models.py
+++ core/models.py
@@ -1,36 +1,13 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.utils.translation import gettext as _
-# This model is for any post that a user posts on the website.
-class Post(models.Model):
-	description = models.CharField(_('Description'), max_length=255, blank=True)
-	image = models.ImageField(_("Image"), upload_to='category_images', null=True, blank=True)
-    User_name = models.ForeignKey(_('Username'), on_delete=models.CASCADE())
-    date_posted = models.DateTimeField(default=timezone.now)
-    def __str__(self):
-        return self.description
+from uuid import uuid4
 
 
-# Comment model links a comment with the post and the user.
-class Comments(models.Model):
-	post = models.ForeignKey('Post', on_delete=models.CASCADE)
-	username = models.ForeignKey(_(User),
-                                 #related_name='details',
-                                 on_delete=models.CASCADE)
-	text = models.CharField(_('Text'), max_length=255)
-	comment_date = models.DateTimeField(default=timezone.now)
+class BaseModel(models.Model):
+    id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
 
 
-    class Meta:
-        verbose_name = _("Comment")
-        verbose_name_plural = _("Comments")
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
 
-    def __str__(self):
-        return f'comment on {self.post.name}'
-
-# It stores the like info. It has the user who created the like and the post on which like was made.
-class Like(models.Model):
-	user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
-	post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse('<h1>Welcome</h1>')
+
+def like(request):
+    pass
+
+def create_post(request):
+    pass
