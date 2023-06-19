7281a61202f33edc3f0a7daa2a0f2ec0afedb2e4
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-06-19 20:54:34 +0330

    add admin of user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Profile, FriendRequest
 
-# Register your models here.
+# admin.site.register(Profile)
+# admin.site.register(FriendRequest)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'is_online', 'bio']
+    list_filter = ['is_seen']
+    search_fields = ['username', 'username']
+
+
+@admin.register(FriendRequest)
+class FriendRequest(admin.ModelAdmin):
+    list_display = ['sender', 'receiver', 'is_active']
+    list_filter = ['is_active']
+
