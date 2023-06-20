cb1cdddc8eecf1d5e562845eaea1ba4a0f22dd45
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-06-20 11:28:29 +0330

    add new method of models

--- user/models.py
+++ user/models.py
@@ -6,103 +6,67 @@ from core.models import BaseModel, TimeStampMixin
 # Register your models here.
 
 
-class Profile(models.Model, BaseModel, TimeStampMixin):
-    username = models.ForeignKey(
+class Profile(TimeStampMixin, BaseModel):
+    username = models.OnetoOnefield(
         User,
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE,
+        unique=True,)
 
     image = models.ImageField(
-        'Image',
         default='default.png',
         upload_to='profile_pics',
         null=True,
         blank=True)
-
     is_online = models.BooleanField(default=False)
     password = models.CharField(
+        _("Password"),
         max_length=12,
-        verbose_name="password",
-        help_text=_('password to login'),
-        null=False,
-        blank=False,
-    )
+        help_text=_('password to login'))
+
     bio = models.CharField(_('Bio'),
                            max_length=255,
                            blank=True,
-                           null=True,)
-
-
-def post_save_user_model_receiver(sender, instance, created):
-    if created:
-        try:
-            Profile.objects.create(user=instance)
-        except:
-            pass
-
+                           null=True)
 
     def profile_posts(self):
-        return self.username.post_set.all()
+        return self.objects.post.all()
 
     def get_friends(self):
         return self.friends.all()
 
     def get_friends_no(self):
-        return self.friends.all().count()
+        return self.friends.count()
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.username} Profile'
 
 
-STATUS_CHOICES = (
-    ('send', 'send'),
-    ('accepted', 'accepted')
-)
+# STATUS_CHOICES = (
+#    ('send', 'send'),
+#   ('accepted', 'accepted')
+# )
 
 
-class FriendList(models.Model):
-    """""FriendList model"""""
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
-    friends = models.ManyToManyField(User, blank=True, related_name='friends')
-
-    def __str__(self):
-        return self.user.username
-
-    def add_friend(self, account):
-        if not account in self.friends.all():
-            self.friends.add(account)
-            self.save()
-
-    def remove_friend(self, account):
-        if account in self.friends.all():
-            self.friends.remove(account)
-            self.save()
-
-    def unfriend(self, remove):
-        remover_friends_list = self
-        remover_friends_list.remove_friend(remove)
-        friends_list = FriendList.objects.get(user=remove)
-        friends_list.remove_friend(self.user)
-
-    def is_mutual_friend(self, friend):
-        if not friend in self.friends.all():
-            return False
-
-
-class FriendRequest(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
-    is_active = models.BooleanField(blank=True, null=True, default=True)
+class FriendRequest(BaseModel):
+    sender = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='sender')
+    receiver = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='receiver')
+    is_active = models.BooleanField(blank=True,
+                                    null=True,
+                                    default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.sender.username
 
     def accept(self):
-        # update both sender and receiver friend list
-        receiver_friend_list = FriendList.objects.get(user=self.receiver)
+        receiver_friend_list = FriendRequest.objects.get(user=self.receiver)
         if receiver_friend_list:
             receiver_friend_list.add_friend(self.sender)
-            sender_friend_list = FriendList.objects.get(user=self.sender)
+            sender_friend_list = FriendRequest.objects.get(user=self.sender)
             if sender_friend_list:
                 sender_friend_list.add_friend(self.receiver)
                 self.is_active = False
@@ -115,3 +79,23 @@ class FriendRequest(models.Model):
     def cancel(self):
         self.is_active = False
         self.save()
+
+    def add_friend(self, account):
+        if not account in self.sender.all():
+            self.sender.add(account)
+            self.save()
+
+    def remove_friend(self, account):
+        if account in self.receiver.all():
+            self.receiver.remove(account)
+            self.save()
+
+    def unfriend(self, remove):
+        remover_friends_list = self
+        remover_friends_list.remove_friend(remove)
+        friends_list = FriendRequest.objects.get(user=remove)
+        friends_list.remove_friend(self.sender)
+
+    def is_mutual_friend(self, friend):
+        if not friend in self.sender.all():
+            return False
