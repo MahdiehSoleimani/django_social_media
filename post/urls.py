7292713e080dde8a38af9b00652d5e8742b88038
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-06-26 17:24:18 +0330

    update urls and added notifications

---
+++ post/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    path('like/', views.like, name='post-like'),
+    path('post/<int:pk>/update/', views.PostUpdateView, name='post-update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post-delete'),
+    path('search_posts/', views.search_posts, name='search_posts'),
+    path('notifications/',views.show_notifications, name='notification')
+]
