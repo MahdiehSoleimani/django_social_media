c4c23aaaeca9ad886ec7f4ec9f01ce85d3534e5c
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-06-26 23:18:56 +0330

    follow and unfollow

--- user/views.py
+++ user/views.py
@@ -1,59 +1,73 @@
+from Tools.demo.mcast import receiver
 from django.shortcuts import render, redirect, get_object_or_404
 from post.models import Post
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Profile, FriendRequest
 from .forms import UserRegisterForm
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib.auth.models import User
 
-User = get_user_model()
+
+@receiver(user_logged_in)
+def got_online(user):
+    user.profile.is_online = True
+    user.profile.save()
+
+
+@receiver(user_logged_out)
+def got_offline(user):
+    user.profile.is_online = False
+    user.profile.save()
 
 
 @login_required
 def delete_friend_request(request, id):
-    from_user = get_object_or_404(User, id=id)
+    from_user = get_object_or_404('User', id=id)
     quest = FriendRequest.objects.filter(from_user=from_user, to_user=request.user).first()
     quest.delete()
-    return HttpResponseRedirect('/users/{}'.format(request.user.profile.slug))
+    return HttpResponseRedirect('/user/{}'.format(request.user.profile.slug))
 
 
-def delete_friend(request, id):
+def unfollow(request, id):
     user_profile = request.user.profile
     friend_profile = get_object_or_404(Profile, id=id)
     user_profile.friends.remove(friend_profile)
     friend_profile.friends.remove(user_profile)
-    return HttpResponseRedirect('/users/{}'.format(friend_profile.slug))
+    return HttpResponseRedirect('/user/{}'.format(friend_profile.slug))
 
 
 @login_required
 def profile_view(request, slug):
-    p = Profile.objects.filter(slug=slug).first()
-    u = p.user
-    sent_friend_requests = FriendRequest.objects.filter(sender=p.user)
-    rec_friend_requests = FriendRequest.objects.filter(receiver=p.user)
-    user_posts = Post.objects.filter(user_name=u)
+    profile = Profile.objects.filter(slug=slug).first()
+    user = profile.user
+    sent_friend_requests = FriendRequest.objects.filter(sender=profile.user)
+    rec_friend_requests = FriendRequest.objects.filter(receiver=profile.user)
+    user_posts = Post.objects.filter(user_name=user)
 
-    friends = p.friends.all()
+    friends = profile.friends.all()
 
     # is this user our friend
-    button_status = 'none'
-    if p not in request.user.profile.friends.all():
-        button_status = 'not_friend'
+    status = 'none'
+    if profile not in request.user.profile.friends.all():
+        status = 'not_friend'
 
         # if we have sent him a friend request
         if len(FriendRequest.objects.filter(
-                sender=request.user).filter(receiver=p.user)) == 1:
-            button_status = 'friend_request_sent'
+                sender=request.user).filter(receiver=profile.user)) == 1:
+            status = 'friend_request_sent'
 
         # if we have received a friend request
         if len(FriendRequest.objects.filter(
-                from_user=p.user).filter(to_user=request.user)) == 1:
-            button_status = 'friend_request_received'
+                sender=profile.user).filter(receiver=request.user)) == 1:
+            status = 'friend_request_received'
 
     context = {
-        'u': u,
-        'button_status': button_status,
+        'u': user,
+        'button_status': status,
         'friends_list': friends,
         'sent_friend_requests': sent_friend_requests,
         'rec_friend_requests': rec_friend_requests,
@@ -63,49 +77,42 @@ def profile_view(request, slug):
     return render(request, "users/profile.html", context)
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You can now login!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+class UserUnfollowView(LoginRequiredMixin, View):
+    def dispatch(self, request, *args, **kwargs):
+        user = User.objects.get(id=kwargs['user_id'])
+        if user.id != request.user.id:
+            return super().dispatch(request)
+        else:
+            # messages.error(request, 'you cant follow/unfollow your account')
+            return redirect('account:user_profile', user.id)
+
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        relation = FriendRequest.objects.filter(sender=request.user, receiver=user)
+        if relation.exists():
+            relation.delete()
+            messages.success(request, 'you unfollowed this user')
+        else:
+            messages.error(request, 'you are not following this user')
+        return redirect('user_profile', user.id)
 
 
-@login_required
-def my_profile(request):
-    p = request.user.profile
-    you = p.user
-    sent_friend_requests = FriendRequest.objects.filter(from_user=you)
-    rec_friend_requests = FriendRequest.objects.filter(to_user=you)
-    user_posts = Post.objects.filter(user_name=you)
-    friends = p.friends.all()
+class UserFollowView(LoginRequiredMixin, View):
+    def dispatch(self, request, *args, **kwargs):
+        user = User.objects.get(id=kwargs['user_id'])
+        if user.id != request.user.id:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            messages.error(request, 'you cant follow/unfollow your account', 'danger')
+            return redirect('profile', user.id)
 
-    # is this user our friend
-    button_status = 'none'
-    if p not in request.user.profile.friends.all():
-        button_status = 'not_friend'
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        relation = FriendRequest.objects.filter(sender=request.user, receiver=user)
+        if relation.exists():
+            messages.error(request, 'you are already following this user', 'danger')
+        else:
+            FriendRequest(sender=request.user, receiver=user).save()
+            messages.success(request, 'you followed this user')
 
-        # if we have sent him a friend request
-        if len(FriendRequest.objects.filter(
-                from_user=request.user).filter(to_user=you)) == 1:
-            button_status = 'friend_request_sent'
 
-        if len(FriendRequest.objects.filter(
-                from_user=p.user).filter(to_user=request.user)) == 1:
-            button_status = 'friend_request_received'
-
-    context = {
-        'u': you,
-        'button_status': button_status,
-        'friends_list': friends,
-        'sent_friend_requests': sent_friend_requests,
-        'rec_friend_requests': rec_friend_requests,
-        'post_count': user_posts.count
-    }
-
-    return render(request, "users/profile.html", context)
