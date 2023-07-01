56a9c32dc445541ba12b63f336144b0cacaa3c70
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-07-01 21:46:38 +0330

    debug views

--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from msilib.schema import ListView
+from django.views import View
 from .forms import NewCommentForm
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse_lazy
@@ -12,7 +13,7 @@ from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-class PostListView(ListView):
+class PostListView(ListView, View):
     model = Post
     template_name = 'home.html'
     context_object_name = 'posts'
@@ -27,41 +28,41 @@ class PostListView(ListView):
         return context
 
 
-@login_required
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    user = request.user
-    is_liked = Reaction.objects.filter(user=user, post=post)
-    if request.method == 'POST':
-        form = NewCommentForm(request.POST)
-        if form.is_valid():
-            data = form.save(commit=False)
-            data.post = post
-            data.username = user
-            data.save()
-            return redirect('post-detail', pk=pk)
-    else:
-        form = NewCommentForm()
-    return render(request, 'feed/post_detail.html', {'post': post, 'is_liked': is_liked, 'form': form})
+# @login_required
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     user = request.user
+#     is_liked = Reaction.objects.filter(user=user, post=post)
+#     if request.method == 'POST':
+#         form = NewCommentForm(request.POST)
+#         if form.is_valid():
+#             data = form.save(commit=False)
+#             data.post = post
+#             data.username = user
+#             data.save()
+#             return redirect('post-detail', pk=pk)
+#     else:
+#         form = NewCommentForm()
+#     return render(request, 'feed/post_detail.html', {'post': post, 'is_liked': is_liked, 'form': form})
 
 
-@login_required
-def create_post(request):
-    user = request.user
-    if request.method == "POST":
-        form = NewPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            data = form.save(commit=False)
-            data.user_name = user
-            data.save()
-            messages.success(request, f"Posted Successfully")
-            return redirect('home')
-    else:
-        form = NewPostForm()
-    return render(request, 'feed/create_post.html', {'form': form})
+# @login_required
+# def create_post(request):
+#     user = request.user
+#     if request.method == "POST":
+#         form = NewPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             data = form.save(commit=False)
+#             data.user_name = user
+#             data.save()
+#             messages.success(request, f"Posted Successfully")
+#             return redirect('home')
+#     else:
+#         form = NewPostForm()
+#     return render(request, 'feed/create_post.html', {'form': form})
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UpdateView, View):
     model = Post
     fields = ['description', 'pic', 'tags']
     template_name = 'create_post.html'
@@ -71,7 +72,7 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
         return super().form_valid(form)
 
 
-class UserPostListView(LoginRequiredMixin):
+class UserPostListView(LoginRequiredMixin, View):
     """
        Displays a list of posts, ordered by the creation date.
     """
@@ -92,15 +93,6 @@ class UserPostListView(LoginRequiredMixin):
     def get_queryset(self):
         user = get_object_or_404('User', username=self.get('username'))
         return Post.objects.filter(user_name=user).order_by('-date_posted')
-
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, ABC):
-    model = Post
-    Fields = ['description', 'image', 'tag']
-
-    def form_valid(self, form):
-        form.instance.user_name = self.request.user
-        return super().form_valid(form)
 
 
 @login_required
@@ -199,15 +191,15 @@ class NotificationListView(LoginRequiredMixin, ListView):
     paginate_by = 10
 
 
-@login_required
-def show_notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(user=user)
-    context = {
-        'user': user,
-        'notification': notifications
-    }
-    return render(request, 'show_notifications', context)
+# @login_required
+# def show_notifications(request):
+#    user = request.user
+#    notifications = Notification.objects.filter(user=user)
+#   context = {
+#       'user': user,
+#       'notification': notifications
+#  }
+#   return render(request, 'show_notifications', context)
 
 
 class ReactionCreateView(LoginRequiredMixin, CreateView):
