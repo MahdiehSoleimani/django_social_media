9856ce0574e6c7ea21982385b91abc44f59e8a94
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-07-01 19:40:36 +0330

    add some help text

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, PermissionsMixin
+from django.utils.itercompat import is_iterable
 from django.utils.translation import gettext as _
 from post import models
 from core.models import BaseModel, TimeStampMixin
@@ -7,11 +8,14 @@ from django.contrib.auth.models import AbstractUser
 # Register your models here.
 
 
-class Profile(TimeStampMixin, BaseModel, AbstractUser, PermissionsMixin):
+class Profile(AbstractUser, PermissionsMixin):
     username = models.OnetoOnefield(
         User,
         on_delete=models.CASCADE,
-        unique=True,)
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.")
+    )
 
     image = models.ImageField(
         upload_to='profile_pics',
@@ -27,14 +31,38 @@ class Profile(TimeStampMixin, BaseModel, AbstractUser, PermissionsMixin):
                            max_length=255,
                            blank=True,
                            null=True)
-    email = models.EmailField()
+    email = models.EmailField(_("email address"), blank=True)
     slug = models.SlugField()
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
 
     def profile_posts(self):
         return self.objects.post.all()
 
     def __str__(self):
         return f'{self.username} Profile'
+
+
+@property
+def user_permissions(self):
+    return self._user_permissions
+
+
+def has_perms(self, perm_list, obj=None):
+    if not is_iterable(perm_list) or isinstance(perm_list, str):
+        raise ValueError("perm_list must be an iterable of permissions.")
+    return all(self.has_perm(perm, obj) for perm in perm_list)
 
 
 # STATUS_CHOICES = (
