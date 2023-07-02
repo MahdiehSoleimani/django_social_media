a61e465eb9b5ea3df9ef3ebd20ba1ec6716d24f3
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-07-02 21:23:24 +0330

    add recycleprofile

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile, FriendRequest
+from .models import Profile, FriendRequest, RecycleProfile
+
 
 # admin.site.register(Profile)
 # admin.site.register(FriendRequest)
@@ -13,7 +14,18 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 @admin.register(FriendRequest)
-class FriendRequest(admin.ModelAdmin):
+class FriendRequestAdmin(admin.ModelAdmin):
     list_display = ['sender', 'receiver', 'is_active']
     list_filter = ['is_active']
 
+
+@admin.register(RecycleProfile)
+class RecycleProfile(admin.ModelAdmin):
+    actions = ['recover']
+
+    def get_queryset(self, request):
+        return RecycleProfile.deleted.filter(is_deleted=True)
+
+    @admin.action(description='recover deleted item')
+    def recover(self, queryset):
+        queryset.update(is_deleted=False, deleted_at=None)
