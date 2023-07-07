49eb3a1d8b736730a5df606382459695f3186cd8
Mahdieh Soleimani <mahdieh.soleimani2000@gmail.com>
2023-07-07 20:29:47 +0330

    update urls and setting

--- social_media/social_media/urls.py
+++ social_media/social_media/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from .settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
-]
+    path('core/', include('core.urls'), namespace='core'),
+    path('post/', include('post.urls', namespace='post')),
+    path('user/', include('user.urls'), namespace='user'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
